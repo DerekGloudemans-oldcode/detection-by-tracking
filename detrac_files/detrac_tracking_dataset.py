3e3cc48ec53c0fc4f0b6a20eedaf8075b1653316
Derek <derekgloudemans@gmail.com>
2020-04-29 11:10:11 -0500

    Add tracking_dataset
    For KF fitting

--- detrac_files/detrac_tracking_dataset.py
+++ detrac_files/detrac_tracking_dataset.py
@@ -4,129 +4,133 @@ Provides:
     - plotting of 2D bounding boxes
     - training/testing loader mode (random images from across all tracks) using __getitem__()
     - track mode - returns a single image, in order, using __next__()
-    
-It is assumed that image dir is a directory containing a subdirectory for each track sequence
-Label dir is a directory containing a bunch of label files
 """
 
 import os
 import numpy as np
+import random 
+import math
+random.seed = 0
 
 import cv2
 from PIL import Image
+import torch
 from torch.utils import data
+from torchvision import transforms
+from torchvision.transforms import functional as F
+
 import xml.etree.ElementTree as ET
+import matplotlib.pyplot as plt
 
+try:
+    from detrac_files.detrac_plot_utils_copy import pil_to_cv, plot_bboxes_2d
+except:
+    from detrac_plot_utils_copy import pil_to_cv, plot_bboxes_2d
 
-from detrac_plot_utils_copy import pil_to_cv, plot_bboxes_2d
 
 class Track_Dataset(data.Dataset):
     """
     Creates an object for referencing the UA-Detrac 2D object tracking dataset
+    and returning single object images for localization. Note that this dataset
+    does not automatically separate training and validation data, so you'll 
+    need to partition data manually by separate directories
     """
     
-    def __init__(self, image_dir, label_dir):
-        """ initializes object. By default, the first track (cur_track = 0) is loaded 
-        such that next(object) will pull next frame from the first track"""
+    def __init__(self, label_dir):
+        """ initializes object
+        image dir - (string) - a directory containing a subdirectory for each track sequence
+        label dir - (string) - a directory containing a label file per sequence
+        """
 
-        # stores files for each set of images and each label
-        dir_list = next(os.walk(image_dir))[1]
-        track_list = [os.path.join(image_dir,item) for item in dir_list]
-        label_list = [os.path.join(label_dir,item) for item in os.listdir(label_dir)]
-        track_list.sort()
-        label_list.sort()
         
-        self.track_offsets = [0]
-        self.track_metadata = []
-        self.all_data = []
-        
-        # parse and store all labels and image names in a list such that
-        # all_data[i] returns dict with image name, label and other stats
-        # track_offsets[i] retuns index of first frame of track[i[]]
-        for i in range(0,len(track_list)):
-
-            images = [os.path.join(track_list[i],frame) for frame in os.listdir(track_list[i])]
-            images.sort() 
-            labels,metadata = self.parse_labels(label_list[i])
-            self.track_metadata.append(metadata)
+        # parse labels and store in dict keyed by track name
+        label_list = []
+        for item in os.listdir(label_dir):
+            name = item.split("_v3.xml")[0]
+            detections = self.parse_labels(os.path.join(label_dir,item))[0]
             
-            for j in range(len(images)):
-                out_dict = {
-                        'image':images[j],
-                        'label':labels[j],
-                        'track_len': len(images),
-                        'track_num': i,
-                        'frame_num_of_track':j
-                        }
-                self.all_data.append(out_dict)
+            objects = {}
             
-            # index of first frame
-            if i < len(track_list) - 1:
-                self.track_offsets.append(len(images)+self.track_offsets[i])
             
-        # for keeping track of things
-        self.cur_track =  None # int
-        self.cur_frame = None
-        self.num_tracks = len(track_list)
-        self.total_num_frames = len(self.all_data)
-        
-        # in case it is later important which files are which
-        self.track_list = track_list
+            for frame in detections:
+                for item in frame:
+                    id = item['id']
+                    bbox = item['bbox']
+                    if id in objects.keys():
+                        objects[id].append(bbox)
+                    else:
+                        objects[id] = [bbox]
+                        
+            # get rid of object ids and just keep list of bboxes
+            for id in objects:
+                label_list.append(np.array(objects[id]))
+                
         self.label_list = label_list
         
-        # load track 0
-        self.load_track(0)
-        
-        
-    def load_track(self,idx):
-        """moves to track indexed by idx (int)."""
-        try:
-            if idx >= self.num_tracks or idx < 0:
-                raise Exception
-                
-            self.cur_track = idx
-            # so that calling next will load frame 0 of that track
-            self.cur_frame = self.track_offsets[idx]-1 
-        except:
-            print("Invalid track number")
+        # parse_labels returns a list (one frame per index) of lists, where 
+        # each item in the sublist is one object
+        # so we need to go through and keep a running record of all objects, indexed by id
             
-    def num_tracks(self):
-        """ return number of tracks"""
-        return self.num_tracks
-    
-    def __next__(self):
-        """get next frame label, and a bit of other info from current track"""
-                
-        self.cur_frame = self.cur_frame + 1
-        cur = self.all_data[self.cur_frame]
-        im = Image.open(cur['image'])
-        label = cur['label']
-        track_len = cur['track_len']
-        frame_num_of_track = cur['frame_num_of_track']
-        track_num = cur['track_num']
-        metadata = self.track_metadata[track_num]
         
-        return im, label, frame_num_of_track, track_len, track_num, metadata
+        
+        
 
 
     def __len__(self):
         """ returns total number of frames in all tracks"""
-        return self.total_num_frames
-    
-    def __getitem__(self,index):
-        """ returns item indexed from all frames in all tracks"""
-        cur = self.all_data[index]
-        im = Image.open(cur['image'])
-        label = cur['label']
+        return len (self.label_list)
 
-        return im, label
-    
+    def __getitem__(self, index, n = 8):
+        
+        data = self.label_list[index]
+        
+        # if track is too short, just use the next index instead
+        while len(data) < n:
+            index += 1
+            data = self.label_list[index]
+        
+        start = np.random.randint(0,len(data)-n)
+        return data[start:start+n,:]
+        
+        
     def parse_labels(self,label_file):
         """
         Returns a set of metadata (1 per track) and a list of labels (1 item per
         frame, where an item is a list of dictionaries (one dictionary per object
         with fields id, class, truncation, orientation, and bbox
         """
+        
+        class_dict = {
+            'Sedan':0,
+            'Hatchback':1,
+            'Suv':2,
+            'Van':3,
+            'Police':4,
+            'Taxi':5,
+            'Bus':6,
+            'Truck-Box-Large':7,
+            'MiniVan':8,
+            'Truck-Box-Med':9,
+            'Truck-Util':10,
+            'Truck-Pickup':11,
+            'Truck-Flatbed':12,
+            
+            0:'Sedan',
+            1:'Hatchback',
+            2:'Suv',
+            3:'Van',
+            4:'Police',
+            5:'Taxi',
+            6:'Bus',
+            7:'Truck-Box-Large',
+            8:'MiniVan',
+            9:'Truck-Box-Med',
+            10:'Truck-Util',
+            11:'Truck-Pickup',
+            12:'Truck-Flatbed'
+            }
+        
+        
         tree = ET.parse(label_file)
         root = tree.getroot()
         
@@ -134,8 +138,8 @@ class Track_Dataset(data.Dataset):
         seq_name = root.attrib['name']
         
         # get list of all frame elements
-        frames = root.getchildren()
-        
+        #frames = root.getchildren()
+        frames = list(root)
         # first child is sequence attributes
         seq_attrs = frames[0].attrib
         
@@ -152,11 +156,20 @@ class Track_Dataset(data.Dataset):
         
         # rest are bboxes
         all_boxes = []
+        frame_counter = 1
         for frame in frames:
+            while frame_counter < int(frame.attrib['num']):
+                # this means that there were some frames with no detections
+                all_boxes.append([])
+                frame_counter += 1
+            
+            frame_counter += 1
             frame_boxes = []
-            boxids = frame.getchildren()[0].getchildren()
+            #boxids = frame.getchildren()[0].getchildren()
+            boxids = list(list(frame)[0])
             for boxid in boxids:
-                data = boxid.getchildren()
+                #data = boxid.getchildren()
+                data = list(boxid)
                 coords = data[0].attrib
                 stats = data[1].attrib
                 bbox = np.array([float(coords['left']),
@@ -166,6 +179,7 @@ class Track_Dataset(data.Dataset):
                 det_dict = {
                         'id':int(boxid.attrib['id']),
                         'class':stats['vehicle_type'],
+                        'class_num':class_dict[stats['vehicle_type']],
                         'color':stats['color'],
                         'orientation':float(stats['orientation']),
                         'truncation':float(stats['truncation_ratio']),
@@ -181,40 +195,20 @@ class Track_Dataset(data.Dataset):
                 'ignored_regions':ignored_regions
                 }
         return all_boxes, sequence_metadata
-    
-    def plot(self,track_idx,SHOW_LABELS = True):
-        """ plots all frames in track_idx as video
-            SHOW_LABELS - if True, labels are plotted on sequence
-            track_idx - int    
-        """
-
-        self.load_track(track_idx)
-        im,label,frame_num,track_len,track_num,metadata = next(self)
-        
-        while True:
-            cv_im = pil_to_cv(im)
-            
-            if SHOW_LABELS:
-                cv_im = plot_bboxes_2d(cv_im,label,metadata['ignored_regions'])
-                
-            cv2.imshow("Frame",cv_im)
-            key = cv2.waitKey(1) & 0xff
-            #time.sleep(1/30.0)
-            
-            if key == ord('q'):
-                break
-            
-            # load next frame
-            im,label,frame_num,track_len,track_num,metadata = next(self)
-            if frame_num == track_len - 1:
-                break
-    
-        cv2.destroyAllWindows()
         
 
 if __name__ == "__main__":
     #### Test script here
-    label_dir = "/home/worklab/Desktop/detrac/DETRAC-Train-Annotations-XML-v3"
-    image_dir = "/home/worklab/Desktop/detrac/DETRAC-all-data"
-    test = Track_Dataset(image_dir,label_dir)
-    test.plot(1)
+    try:
+        test
+    except:
+        try:
+            label_dir = "C:\\Users\\derek\\Desktop\\UA Detrac\\DETRAC-Train-Annotations-XML-v3"
+            test = Track_Dataset(label_dir)
+    
+        except:
+            label_dir = "/home/worklab/Desktop/detrac/DETRAC-Train-Annotations-XML-v3"
+            test = Track_Dataset(label_dir)
+    idx = np.random.randint(0,len(test))
+    
+    cv2.destroyAllWindows()
